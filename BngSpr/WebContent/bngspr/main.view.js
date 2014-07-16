280a3220a64007fc995d5d7d49df671e63125cde
Rakhesh R <rksh77@gmail.com>
2014-07-16 21:06:38 +0530

    Project creation
    Project creation

---
+++ BngSpr/WebContent/bngspr/main.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("bngspr.main", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf bngspr.main
+	*/ 
+	getControllerName : function() {
+		return "bngspr.main";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf bngspr.main
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
